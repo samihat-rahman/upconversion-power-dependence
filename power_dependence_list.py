95ab9edea9ba4a7b77af34680d7d4f088d92303d
samihat-rahman <135351908+samihat-rahman@users.noreply.github.com>
2023-06-02 13:37:28 -0400

    Update power_dependence_list.py

--- power_dependence_list.py
+++ power_dependence_list.py
@@ -1,6 +1,7 @@
-# power_dependence_list.py: list the possible powers and randomize the list for power dependence measurements
-# May 31st, 2023 by Samihat Rahman
+# power_dependence_list.py: list the possible powers spaced by a factor and randomize the list for power dependence measurements
+# June 2nd, 2023 by Samihat Rahman
 import datetime
+import numpy as np
 import pandas as pd
 import random as rand
 
@@ -8,13 +9,19 @@ import random as rand
 max_power = 71.2
 min_power = 0.6
 factor = 1.3 
-save = False
+
+# number of trials
+trials = 3
+
+# if you want to save as csv
+save = True
 
 # recursive function that spits out the list
 def powers(max_power, min_power, factor):
     # base case
     if max_power <= min_power:
         return [max_power]
+    # recursive addition
     return [max_power] + powers(max_power/factor, min_power, factor)
     
 # calling the function
@@ -23,23 +30,28 @@ powers_list = powers(max_power, min_power, factor)
 # round to three decimal points
 powers_list = [round(i, 3) for i in powers_list]
 
-# automatically pull the current date for naming the file
+# create a data frame to save the data
+dataframe = pd.DataFrame()
+trials_list = np.arange(1, trials+1)
+
+# shuffle and save to each trial
+for trial in trials_list:
+    power_list_temp = powers_list.copy()
+    rand.shuffle(power_list_temp)
+    dataframe[f'Trial {trial}'] = power_list_temp
+
+# for a column with the powers arranged least to greatest
+reversed_powers = powers_list.copy()
+reversed_powers.reverse()
+
+dataframe['Powers'] = reversed_powers
+
+# final data frame
+print(dataframe)
+
+# save by date of creation
 date = datetime.datetime.now()
 date_string = date.strftime("%Y-%m-%d").replace('/', '-')
 
-# create copies of the list for trials
-powers_list_1 = powers_list.copy()
-powers_list_2 = powers_list.copy()
-powers_list_3 = powers_list.copy()
-
-# shuffle each list
-rand.shuffle(powers_list_1)
-rand.shuffle(powers_list_2)
-rand.shuffle(powers_list_3)
-
-# dataframe which stores the shuffled powers
-dataframe = pd.DataFrame({'Trial 1': powers_list_1, 'Trial 2': powers_list_2, 'Trial 3': powers_list_3})
-
-# save the dataframe
 if save:
     dataframe.to_csv(f'{date_string} List of Powers.csv', index=False)
